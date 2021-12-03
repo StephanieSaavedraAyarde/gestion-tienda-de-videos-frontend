c8a0530570310e6b865c31a6a714de48411fac41
Stephanie Saavedra Ayarde <stephanie.saavedra.ayarde@gmail.com>
2021-12-02 20:46:06 -0400

    Checkout

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -8,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class CheckoutComponent implements OnInit {
 
   public cartItemList: [];
+  items: any;
 
   constructor() { }
 
   ngOnInit(): void {
+
     this.cartItemList = JSON.parse(localStorage.getItem('carrito'));
+    this.items = this.cartItemList.length;
     console.log(this.cartItemList);this.cartItemList.length
     console.log()
   }
